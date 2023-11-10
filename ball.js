21868683cbef9a7100ef9291ccf385f18940313b
Gamze Kirmit <gamze_kirmit@outlook.com>
2023-11-10 19:48:33 +0300

    ball

--- ball.js
+++ ball.js
@@ -1,31 +1,35 @@
-let userName = prompt("What is your name");
-userName
-? console.log(`Hello, ${userName}!`)
-: console.log(`Hello!`);
 
-const userQuestion ="What is my future?";
-console.log(userQuestion);
+        let userName = prompt("What is your name");
+        userName
+        ? document.write(`Hello, ${userName}!`)
+        : document.write(`Hello!`);
 
-const randomNumber = Math.floor(Math.random()*8);
-let eightBall = ""
+        const userQuestion = " What is my future?";
+        document.write(userQuestion);
 
-if(randomNumber===0){
-  eightBall='It is certain';
-}else if(randomNumber===1){
-  eightBall='It is decidedly so';
-}else if(randomNumber===2){
-  eightBall='Reply hazy try again';
-}else if(randomNumber===3){
-  eightBall='Cannot predict now';
-}else if(randomNumber===4){
-  eightBall='Do not count on it';
-}else if(randomNumber===5){
-  eightBall='My sources say no';
-}else if(randomNumber===6){
-  eightBall='Outlook not so good';
-}else if(randomNumber===7){
-  eightBall='Signs point to yes';
-}else{
-  eightBall='undefined';
+    
+    function askEightBall() {
+    const randomNumber = Math.floor(Math.random() * 8);
+    let eightBall = ""
+
+    if (randomNumber === 0) {
+        eightBall = 'It is certain';
+    } else if (randomNumber === 1) {
+        eightBall = 'It is decidedly so';
+    } else if (randomNumber === 2) {
+        eightBall = 'Reply hazy try again';
+    } else if (randomNumber === 3) {
+        eightBall = 'Cannot predict now';
+    } else if (randomNumber === 4) {
+        eightBall = 'Do not count on it';
+    } else if (randomNumber === 5) {
+        eightBall = 'My sources say no';
+    } else if (randomNumber === 6) {
+        eightBall = 'Outlook not so good';
+    } else if (randomNumber === 7) {
+        eightBall = 'Signs point to yes';
+    } else {
+        eightBall = 'undefined';
+    }
+    document.write(` ${randomNumber} - ${eightBall}`);
 }
-console.log(randomNumber ,"-",eightBall);
